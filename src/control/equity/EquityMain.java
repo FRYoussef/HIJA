c3b837b753b0dcd9b16984353fa37e484e292bef
PJColombo <33203511+PJColombo@users.noreply.github.com>
2017-12-15 11:23:59 +0100

    Add files via upload
    Agregado correcto cierre de hilos al cerrar la aplicacion

--- src/control/equity/EquityMain.java
+++ src/control/equity/EquityMain.java
@@ -1,16 +1,18 @@
 package control.equity;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class EquityMain extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-
+    public void start(Stage primaryStage){
         try
         {
             AnchorPane root = FXMLLoader.load(getClass().getResource("../../view/equity.fxml"));
@@ -19,13 +21,18 @@ public class EquityMain extends Application {
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);
             primaryStage.show();
-
+        	primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+        	       @Override
+        	       public void handle(WindowEvent e) {
+        	          Platform.exit();
+        	          System.exit(0);
+        	       }
+        	    });
         }
         catch (Exception e){
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) { launch(args); }
 
 }
