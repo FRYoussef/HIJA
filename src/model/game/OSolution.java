1bd5bccd5ceb05f6f86e5e8327dc4a1e2f9b03ca
PJColombo <33203511+PJColombo@users.noreply.github.com>
2017-12-15 12:08:32 +0100

    Add files via upload

--- src/model/game/OSolution.java
+++ src/model/game/OSolution.java
@@ -9,4 +9,16 @@ public class OSolution extends Observable{
 	    setChanged();
 	    notifyObservers(solution);
     }
+	
+	//--------------PR3
+	
+	public void notifyEquity(double[] is){
+		this.setChanged();
+		this.notifyObservers(is);
+	}
+	
+	public void notifySimulations(int sims){
+		this.setChanged();
+		this.notifyObservers(sims);
+	}
 }
