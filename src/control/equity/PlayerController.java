6a4a46bf7001514faa8ae8f3dc3944345c7b7032
PJColombo <33203511+PJColombo@users.noreply.github.com>
2017-12-17 14:24:19 +0100

    Add files via upload

--- src/control/equity/PlayerController.java
+++ src/control/equity/PlayerController.java
@@ -1,7 +1,5 @@
 package control.equity;
 
-
-
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
@@ -9,93 +7,137 @@ import control.ObserverPatron.HandlerObserver;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import model.representation.Card;
 import model.representation.game.Deck;
 
-
-public class PlayerController{
-
-    private static final DecimalFormat DF = new DecimalFormat("#.###");
-    private static final String DEFAULT_EQUITY = "0%";
-    private static final String EQUITY = "Equity: ";
-    private static final String OUT_TEXT = "Out";
-
-    @FXML
-    private ImageView _ivCard1;
-    @FXML
-    private ImageView _ivCard2;
-    @FXML
-    private Label _lbEquity;
-    @FXML
-    private Button _btFold;
-    @FXML
-    private AnchorPane _playerAnchor;
-
-    private int numPlayer;
-    private Card cd1 = null;
-    private Card cd2 = null;
-    private Deck deck = null;
-    private Stage stageCardsSelec = null;
-    private CardSelectorController cs;
+public abstract class PlayerController {
+	protected static final DecimalFormat DF = new DecimalFormat("#.###");
+    protected static final String DEFAULT_EQUITY = "0%";
+    protected static final String EQUITY = "Equity: ";
+    protected static final String OUT_TEXT = "Out";
     
+    @FXML
+    protected Label _lbEquity;
+    @FXML
+    protected Button _btFold;
+    @FXML
+    protected AnchorPane _playerAnchor;
+    @FXML
+    protected Label _lbPlayer;
+    
+    @FXML
+    protected GridPane _gpCardSet;
+    
+    protected int numPlayer;
+    protected Deck deck = null;
+    protected Stage stageCardsSelec = null;
+    protected CardSelectorController cs;
+   
+    protected ArrayList<Card> cards;
+    protected ImageView[] picCards; 
+    
+    public PlayerController(){
 
-    public void init(int numPlayer, Card card1, Card card2, Stage stageCardsSelec, Deck deck, CardSelectorController cs){
+    }
+    
+    public void init(int numPlayer, ArrayList<Card> cards, Stage stageCardsSelec, Deck deck, CardSelectorController cs){
         this.numPlayer = numPlayer;
-        this.cd1 = card1;
-        this.cd2 = card2;
+        writePlayer(numPlayer);
+        this.cards = cards;
         this.stageCardsSelec = stageCardsSelec;
         this.deck = deck;
         this.cs = cs;
-        if(cd1 != null)
-            _ivCard1.setImage(new Image("resources/cards/" + cd1.toString() + ".png"));
-        if(cd2 != null)
-            _ivCard2.setImage(new Image("resources/cards/" + cd2.toString() + ".png"));
-    }
-
+        init();
+        if(this.cards == null)
+        	this.cards = new ArrayList<>(picCards.length);
+        
+     }
     public int getNumPlayer() {
         return numPlayer;
     }
 
-    public Card getCd1() {
-        return cd1;
+    public Card getCard(int i){
+    	return cards.get(i);
     }
 
-    public Card getCd2() {
-        return cd2;
+    public void setCards(ArrayList<Card> c){
+    	if(c != null){
+    		cards.clear();
+    		for (int i = 0; i < picCards.length; i++) {
+    			cards.add(i, c.get(i));
+				picCards[i].setImage(new Image("resources/cards/" + c.get(i).toString() 
+						+ ".png"));
+			}
+    	}
+    	else{
+    		for (int i = 0; i < picCards.length; i++)
+				picCards[i].setImage(null);
+    	}
     }
-
-    public void setCards(Card card1, Card card2){
-        this.cd1 = card1;
-        this.cd2 = card2;
-
-        if(cd1 != null) _ivCard1.setImage(new Image("resources/cards/" + cd1.toString() + ".png"));
-        else            _ivCard1.setImage(null);
-
-        if(cd2 != null) _ivCard2.setImage(new Image("resources/cards/" + cd2.toString() + ".png"));
-        else            _ivCard2.setImage(null);
+    
+    public void disableForSim(boolean b){
+    	for (ImageView i : picCards)
+    		i.setDisable(b);
+        _btFold.setDisable(b);
     }
-
+    
+    public void clear(){
+        Platform.runLater(() -> {
+        	cards.clear();
+            _lbEquity.setText(EQUITY+DEFAULT_EQUITY);
+            _playerAnchor.setDisable(false);
+            if(_btFold.isDisabled())
+                _btFold.setDisable(false);
+            for (ImageView i : picCards) 
+				i.setImage(null);
+        });
+    }
+    
+    protected void writePlayer(int numPlayer){
+    	Platform.runLater(() ->{
+        	_lbPlayer.setText("Player " + numPlayer);
+        });
+    }
+    
+    @Override
+    public int hashCode() {
+        return numPlayer;
+    }
+    
+    public void writeEquity(double equity){
+    	Platform.runLater(()->{
+    		String number = DF.format(equity*100.f);
+    		_lbEquity.setText(EQUITY + number + '%');
+    	});
+    }
+    
+    public void onClickFold(ActionEvent actionEvent) {
+        //notify the end of this player
+        _playerAnchor.setDisable(true);
+        _lbEquity.setText(OUT_TEXT);
+        HandlerObserver.getoSolution().notifyFold(numPlayer);
+    }
+    
     public void onClickCard(MouseEvent mouseEvent) {
         //card selection
     	Platform.runLater(() -> {
             try {
                 if(!stageCardsSelec.isShowing()) {
-                    ArrayList<Card> cards = null;
-                    if(cd1 != null){
-                        cards = new ArrayList<>(2);
-                        cards.add(cd1);
-                        cards.add(cd2);
+                    ArrayList<Card> c = null;
+                    if(cards != null){
+                        c = new ArrayList<>(cards.size());
+                        for (int i = 0; i < cards.size(); i++)
+							c.add(cards.get(i));
                     }
-                    cs.update(numPlayer, 2, cards);
+                    cs.update(numPlayer, picCards.length, c);
                     stageCardsSelec.show();
                 }
              }
@@ -105,43 +147,6 @@ public class PlayerController{
             }
     	 });
     }
-
-    public void onClickFold(ActionEvent actionEvent) {
-        //notify the end of this player
-        _playerAnchor.setDisable(true);
-        _lbEquity.setText(OUT_TEXT);
-        HandlerObserver.getoSolution().notifyFold(numPlayer);
-    }
-
-    public void clear(){
-        Platform.runLater(() -> {
-            cd1 = null;
-            cd2 = null;
-            _lbEquity.setText(EQUITY+DEFAULT_EQUITY);
-            _playerAnchor.setDisable(false);
-            if(_btFold.isDisabled())
-                _btFold.setDisable(false);
-            _ivCard1.setImage(null);
-            _ivCard2.setImage(null);
-        });
-    }
-
-    @Override
-    public int hashCode() {
-        return numPlayer;
-    }
-
-    public void disableForSim(boolean b){
-        _ivCard1.setDisable(b);
-        _ivCard2.setDisable(b);
-        _btFold.setDisable(b);
-    }
-
-    public void writeEquity(double equity){
-    	Platform.runLater(()->{
-    		String number = DF.format(equity*100.f);
-    		_lbEquity.setText(EQUITY + number + '%');
-    	});
-    }
-
+    
+    protected abstract void init();
 }
